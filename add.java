72ab7a92311619bf76c6eebd015906cc69340da6
Hemalatha CG <hemalathacg92@gmail.com>
2017-08-19 11:14:32 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class add{
+public static void main(String args[]){
+int a=10;
+int b=20;
+int c;
+c=a+b;
+System.out.println("Addition of 2 numbers :"+c);
+}
+}
+flipcart -eCommere>> add.java
+
+
+
+echo flipcart -eCommere
